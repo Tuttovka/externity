6027a7688d938c53d94f09c056241fc21b241231
Martin Kazda <makazda90@gmail.com>
2020-08-14 11:55:01 +0200

    Adding contact in nav and styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,8 +5,11 @@ import logo from '../logo.png';
 export function Navigation() {
     return (
         <div className="nav">
-            <div className="container-logo">
-                    <a href="/"><img src={logo} class="ext-logo" alt="externity logo"/></a>
+            <div>
+                <a href="/"><img src={logo} class="ext-logo" alt="externity logo"/></a>
+            </div>
+            <div>
+                <a href="/contact-form" class="contact-link-header">Contact us</a>
             </div> 
         </div>
 
