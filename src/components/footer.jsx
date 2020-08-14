672c5dd6b0dad81b7d440c77780a7bee9b8a38fd
Martin Kazda <makazda90@gmail.com>
2020-08-14 17:42:48 +0200

    Formating

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-
+import React from "react";
 
 export function Footer() {
-    return (
-        <div className="footer-bar">
-            <a href="/contact-form" class="contact-link-footer">Contact us</a>        
-        </div>
-    );
+  return (
+    <div className="footer-bar">
+      <a href="/contact-form" class="contact-link-footer">
+        Contact us
+      </a>
+    </div>
+  );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import logo from '../logo.png';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import logo from "../logo.png";
 
 export function Navigation() {
-    return (
-        <div className="nav">
-            <div>
-                <a href="/"><img src={logo} class="ext-logo" alt="externity logo"/></a>
-            </div>
-            <div>
-                <a href="/contact-form" class="contact-link-header">Contact us</a>
-            </div> 
-        </div>
-    );
+  return (
+    <div className="nav">
+      <div>
+        <a href="/">
+          <img src={logo} class="ext-logo" alt="externity logo" />
+        </a>
+      </div>
+      <div>
+        <a href="/contact-form" class="contact-link-header">
+          Contact us
+        </a>
+      </div>
+    </div>
+  );
 }
-
