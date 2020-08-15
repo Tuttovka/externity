0c0b8d691574c3abcb11e84099a429f7d2bf7ce1
Martin Kazda <makazda90@gmail.com>
2020-08-15 12:44:51 +0200

    Styling form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,30 +4,20 @@ export function Contact() {
   return (
     <div className="contact-bar">
       <form action="" className="contact-form">
-        <div className="form-bar-top">
-          <div>
-            <label>Name</label>
-            <input required type="text" placeholder="John Doe"></input>
-          </div>
-          <div>
-            <label>E-mail</label>
-            <input
-              required
-              type="email"
-              placeholder="John.Doe@gmail.com"
-            ></input>
-          </div>
-        </div>
-        <div class="form-bar-bottom">
-          <label>Your question</label>
-          <textarea
-            required
-            rows="4"
-            placeholder="Dear Sir or Madam. I would like to aks you.... "
-          ></textarea>
+        <label>Name</label>
+        <input required type="text" placeholder="John Doe"></input>
 
-          <button type="submit">Send</button>
-        </div>
+        <label>E-mail</label>
+        <input required type="email" placeholder="John.Doe@gmail.com"></input>
+
+        <label>Your question</label>
+        <textarea
+          required
+          rows="4"
+          placeholder="Dear Sir or Madam. I would like to aks you.... "
+        ></textarea>
+
+        <button type="submit">Send</button>
       </form>
     </div>
   );
