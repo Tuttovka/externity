ac64e2ce0023f32218d2b877aaa59a3cbcea5a40
Martin Kazda <makazda90@gmail.com>
2020-08-20 09:25:16 +0200

    Contact form saving data in localStorage

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,24 +1,94 @@
 import React from "react";
 
+function mergeWithExpiry({ localStorageKey, key, value, ttl }) {
+  const now = new Date();
+
+  const item = {
+    [key]: value,
+    expire: now.getTime() + ttl,
+  };
+
+  const getPreviousData = getWithExpiry(localStorageKey);
+  localStorage.setItem(
+    localStorageKey,
+    JSON.stringify({ ...getPreviousData, ...item })
+  );
+}
+
+function getWithExpiry(localStorageKey) {
+  const itemStr = localStorage.getItem(localStorageKey);
+
+  if (!itemStr) {
+    return null;
+  }
+  const item = JSON.parse(itemStr);
+
+  const now = new Date();
+  if (now.getTime() > item.expire) {
+    localStorage.removeItem(localStorageKey);
+    return null;
+  }
+  return item;
+}
+
 export function Contact() {
+  const savedFormData = getWithExpiry("formData");
+  const [formData, setFormData] = React.useState(savedFormData || {});
+
+  const setData = function ({ key, value }) {
+    setFormData(value);
+    mergeWithExpiry({
+      localStorageKey: "formData",
+      key,
+      value,
+      // 24 hours
+      ttl: 3600 * 1000 * 24,
+    });
+  };
+
   return (
     <div className="contact-bar">
       <h1>Welcome to our contact form</h1>
       <form action="" className="contact-form">
-        <label>Name</label>
-        <input required type="text" placeholder="John Doe"></input>
+        <label htmlFor="name">Name</label>
+        <input
+          required
+          type="text"
+          id="name"
+          placeholder="John Doe"
+          value={formData.name}
+          onChange={(event) => {
+            setData({ key: "name", value: event.target.value });
+          }}
+        />
 
-        <label>E-mail</label>
-        <input required type="email" placeholder="John.Doe@gmail.com"></input>
+        <label htmlFor="email">E-mail</label>
+        <input
+          required
+          type="email"
+          id="email"
+          placeholder="John.Doe@gmail.com"
+          value={formData.email}
+          onChange={(event) => {
+            setData({ key: "email", value: event.target.value });
+          }}
+        />
 
-        <label>Your question</label>
+        <label htmlFor="question">Your question</label>
         <textarea
           required
           rows="4"
+          id="question"
           placeholder="Dear Sir or Madam. I would like to aks you.... "
+          value={formData.question}
+          onChange={(event) => {
+            setData({ key: "question", value: event.target.value });
+          }}
         ></textarea>
 
-        <button className="contact-button" type="submit">Send</button>
+        <button className="contact-button" type="submit">
+          Send
+        </button>
       </form>
     </div>
   );
